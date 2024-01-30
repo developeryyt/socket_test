fe7cece459c6369d35566d39d399c86787af6f20
developeryyt <yeontaey92@gmail.com>
2024-01-30 11:26:56 +0900

    revise

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 
+
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ['GET', 'POST']
-    },
-    path: '/socket.io',
-})
+const io = new Server(server)
 
 
 
